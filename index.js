b31c26f8e7e331af11f41a705a93d0d8a323dc51
qwerty4882 <harshchoudhary1227@gmail.com>
2023-06-03 17:04:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const io = require('socket.io')(4000, {
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+})
+io.on('connection', (socket) => {
+  socket.on('snd-msg', (message, room) => {
+    // console.log(message, room)
+    socket.to(room).emit('res-msg', message)
+  })
+  socket.on('join-room', (room, cb) => {
+    let clientno = 0
+    if (!io.sockets.adapter.rooms.get(room)) {
+      clientno = 0
+    } else {
+      clientno = io.sockets.adapter.rooms.get(room).size + 1
+    }
+
+    if (clientno <= 2) {
+      socket.join(room)
+      cb(`joined room  ${room}`, clientno)
+      if (clientno === 2) {
+        socket.to(room).emit('res-msg', -1, true)
+      }
+    } else {
+      cb(`room full ${room}`, clientno)
+    }
+  })
+  socket.on('leave-room', (room) => {
+    socket.leave(room)
+  })
+})
