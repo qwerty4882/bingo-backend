6db6515d9ccac4e5bad99a9b6f2e1ae2083b92af
qwerty4882 <87907542+qwerty4882@users.noreply.github.com>
2023-06-03 18:37:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT ||4000
 
 const io = require('socket.io')(PORT, {
   cors: {
-    origin: ['https://bingoonline.netlify.app/'],
+    origin: ["http://http://localhost:3000/"],
   },
 })
 io.on('connection', (socket) => {
